9747718876fba80f30d039dbee7a5ec55033c206
naoki-00-ito <na.o.ki.00.i.to@gmail.com>
2022-11-17 19:14:38 +0900

    19章

---
+++ ruby_ex/ex1902.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+require 'pstore'
+
+# PStoreデータベースをオープンする
+db = PStore.new('fruitdb')
+
+# PStoreが読み込みモードのときに
+# 書き込もうとするとエラーになる
+db.transaction(true) do
+  db["drink"] = "orange juice"
+end
